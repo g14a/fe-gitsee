31201179a21ae7df57e5d525fcf99d2b62acda45
Gowtham Munukutla <gowtham.m81197@gmail.com>
2020-08-12 15:44:34 +0530

    trying to fix location issue in profile component
    Signed-off-by: Gowtham Munukutla <gowtham.m81197@gmail.com>

--- src/components/Constants.js
+++ src/components/Constants.js
@@ -1,1 +1,2 @@
-export const httpURL = process.env.REACT_APP_HEROKU_URL
+//export const httpURL = process.env.REACT_APP_HEROKU_URL
+export const httpURL = `http://localhost:8000/user/`
--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
 import Axios from 'axios';
-import  * as URL from './Constants'
+import * as URL from './Constants'
 
 class ProfileComponent extends Component {
 
@@ -17,6 +17,14 @@ class ProfileComponent extends Component {
   componentDidMount() {
     Axios.get(URL.httpURL + this.props.username)
       .then(response => {
+        if (response.data.location === "") {
+          this.setState({
+            data: {
+              ...this.state.data,
+              location: ""
+            }
+          })
+        }
         this.setState({
           data: response.data
         })
@@ -24,6 +32,7 @@ class ProfileComponent extends Component {
   }
 
   render() {
+
     return (
       <Card
         style={{
@@ -39,7 +48,7 @@ class ProfileComponent extends Component {
         />
         <Card.Body>
           <Card.Text style={{ fontSize: "18px", color: "white" }}>
-            {this.state.data.username} 
+            {this.state.data.username}
           </Card.Text>
           <Card.Subtitle style={{ fontSize: "13px", color: "white" }}>
             <AccessTimeIcon style={{ fontSize: 'medium' }} />
@@ -47,12 +56,12 @@ class ProfileComponent extends Component {
           </Card.Subtitle>
           <Card.Text style={{ fontSize: '13px', marginTop: '10px' }}>
             <LocationOnIcon style={{ fontSize: 'medium' }} />
-          {" "}{this.state.data.location}
+            {" "}{this.state.data.location}
           </Card.Text>
           <Card.Text style={{ fontSize: "14px", color: "white" }}>
             {this.state.data.followers} followers
           </Card.Text>
-          <Card.Link href={this.state.data.url} target="_blank" style={{ fontSize: '18px', display: 'block'}}> Visit on Github </Card.Link>
+          <Card.Link href={this.state.data.url} target="_blank" style={{ fontSize: '18px', display: 'block' }}> Visit on Github </Card.Link>
         </Card.Body>
 
       </Card>
